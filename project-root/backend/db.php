ea4f846652deb90bd11f1968b490bb455ea07e8d
pasindumanahara <manahara2003@gmail.com>
2026-01-04 12:50:46 +0530

    Add db.php file (completed) , login.php
    db.php written with PDO better safe way

--- project-root/backend/db.php
+++ project-root/backend/db.php
@@ -1,2 +1,20 @@
-<?php //TODO:db access ?>
+<?php  
+    $DB_HOST = "";
+    $DB_NAME = "";
+    $DB_USER = "";
+    $DB_PASSWORD = "";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+        $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASSWORD, $options);
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo "Database connection failed: " . htmlspecialchars($e->getMessage());
+        exit;
+    }
+
+?>
 

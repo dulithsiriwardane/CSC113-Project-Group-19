fc5bb7cea8fa0afcad6abb0c995b6f5082cc34ca
Pasindu Manahara <manahara2003@gmail.com>
2026-01-14 09:44:06 +0530

    Update sessionData.php

--- project-root/sessionData.php
+++ project-root/sessionData.php
@@ -12,4 +12,25 @@
         "name"  =>  $_SESSION['name']
     ]);
     
+    //////////////
+    <?php
+session_start();
+header("Content-Type: application/json");
+
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode([
+        "loggedIn" => false
+    ]);
+    exit;
+}
+
+echo json_encode([
+    "loggedIn" => true,
+    "user" => [
+        "id" => $_SESSION['user_id'],
+        "name" => $_SESSION['username'],
+        "role" => $_SESSION['role']
+    ]
+]);
+//////:
 ?>
